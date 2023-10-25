7d531da5973044f954e23986bd48d622860ed5a2
zadarmo <wx01290804@163.com>
2023-10-26 02:02:33 +0800

    init project

---
+++ src/main/java/com/example/webswipeservice/WebSwipeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.webswipeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSwipeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSwipeServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webswipeservice/WebSwipeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webswipeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSwipeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
