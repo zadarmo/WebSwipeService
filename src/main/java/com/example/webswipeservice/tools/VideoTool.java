0225c588c16ed96df5d653e2e9150068552b0dd8
zadarmo <wx01290804@163.com>
2023-10-30 15:25:19 +0800

    refactor: rename function

--- src/main/java/com/example/webswipeservice/tools/VideoTool.java
+++ src/main/java/com/example/webswipeservice/tools/VideoTool.java
@@ -6,16 +6,17 @@ import com.qiniu.util.Auth;
 
 public class VideoTool {
     /**
-     * 从七牛云下载一个视频
+     * 生成七牛云的资源访问外链
      * @param domain 域名
      * @param useHttps 是否使用https协议
      * @param key 视频key
      * @param expireInSeconds 有效期, 单位为秒
      * @param accessKey 公钥
      * @param secretKey 私钥
-     * @return 视频的url
+     * @return 资源外链
      */
-    public static String downloadVideoFromQly(String domain, boolean useHttps, String key, long expireInSeconds, String accessKey, String secretKey) throws QiniuException {
+
+    public static String buildQlySrcUrl(String domain, boolean useHttps, String key, long expireInSeconds, String accessKey, String secretKey) throws QiniuException {
         // domain   下载 domain, eg: qiniu.com【必须】
         // useHttps 是否使用 https【必须】
         // key      下载资源在七牛云存储的 key【必须】
